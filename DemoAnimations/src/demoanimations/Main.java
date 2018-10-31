dc554e9aa4c59e50517ed161eca787c07a37657b
Patryk Smith <pjs72@psu.edu>
2018-10-30 21:28:40 -0400

    Created Demo Animations

---
+++ DemoAnimations/src/demoanimations/Main.java
@@ -0,0 +1,27 @@
+package demoanimations;
+
+import javax.swing.*;
+
+/**
+ * @author Patryk Smith
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        TimerDemo1 prog = new TimerDemo1();
+        prog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        prog.setVisible(true);
+    }
+    
+}
+
+
+
+
