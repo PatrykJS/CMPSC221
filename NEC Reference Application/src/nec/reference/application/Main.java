712228237fb780caf0ead66822ec15a75d1a7ed7
Patryk Smith <pjs72@psu.edu>
2018-11-29 00:00:48 -0500

    Created RJ's App

---
+++ NEC Reference Application/src/nec/reference/application/Main.java
@@ -0,0 +1,20 @@
+/*
+ */
+package nec.reference.application;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Patryk Smith
+ */
+public class Main {
+    public static void main(String[] args){
+        
+        ApplicationWindow app = new ApplicationWindow();
+        //app.setSize(1080,300);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("RJ's Computational Electrical Device");
+    }
+}
