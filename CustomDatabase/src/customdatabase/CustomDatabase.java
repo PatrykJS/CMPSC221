9a2c75e0102ec221422f7cc17927c2f56661bf28
Patryk Smith <pjs72@psu.edu>
2018-12-03 18:19:46 -0500

    Created

---
+++ CustomDatabase/src/customdatabase/CustomDatabase.java
@@ -0,0 +1,27 @@
+/*
+ */
+package customdatabase;
+
+/**
+ *
+ * @author Patryk Smith
+ */
+public class CustomDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Application app = new Application();
+        app.setTitle("Hi there.");
+        app.setSize(400, 400);
+        app.setVisible(true);
+         
+        
+        System.out.println("Goodbye!");
+
+    }
+}
