aef24758c162096baae73f27b7d303549151844e
Patryk Smith <pjs72@psu.edu>
2018-12-13 23:21:47 -0500

    created new Database

--- CustomDatabase/src/customdatabase/Main.java
+++ CustomDatabase/src/customdatabase/Main.java
@@ -13,11 +13,17 @@ public class Main {
     
     public static void main(String[] args) throws SQLException{
          
-        ApplicationWindow app = new ApplicationWindow(); 
+        MainScreen app = new MainScreen(); 
+        app.setSize(600,600);
+         
+        app.add(MainScreen.deleteWindow);
+        app.add(MainScreen.insertWindow);
+        app.add(MainScreen.searchWindow);
         
+        app.setLocationRelativeTo(null);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
-        app.setTitle("Leber & Bonham Electrical Inc");
+        app.setTitle("Custom Database");
         
     }
 }
